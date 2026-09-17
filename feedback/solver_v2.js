51f60dd7bbaf1b824028756310528eb98fb66a53
井0A <s60046s60046@gmail.com>
2026-09-18 07:37:46 +0800

    Expand Taiwan outline from 140 to 146 degrees

--- feedback/solver_v2.js
+++ feedback/solver_v2.js
@@ -5,7 +5,7 @@ import {
   sphericalTriangleArea
 } from "./geo.js";
 
-export const TARGET_MAX_COLAT_DEG = 140;
+export const TARGET_MAX_COLAT_DEG = 146;
 export const FINAL_AUDIT_EDGE_DEG = 1.5;
 const QUICK_AUDIT_EDGE_DEG = 4.5;
 const NANTOU_AREA_RETAIN = 1.00;
@@ -541,6 +541,6 @@ export function buildSolvedProjection(features) {
 
   p.towns = towns;
   p.feedbackPasses = FEEDBACK_PASSES;
-  p.policyVersion = "outline140+priority10+visible-boost-v2+high-outlier-donor1.3+xinyi-local1.2";
+  p.policyVersion = "outline146+priority10+visible-boost-v2+high-outlier-donor1.3+xinyi-local1.2";
   return p;
 }
