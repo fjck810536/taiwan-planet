220e813fec63e4894f2f34f523b5a05a61dfe48f
井0A <s60046s60046@gmail.com>
2026-09-18 10:54:07 +0800

    Add editable ultralite region palette

---
+++ feedback/palette.js
@@ -0,0 +1,16 @@
+// Edit only this file to recolor regions after the map has been baked.
+// Keys are stable region ids. Examples:
+// "台北市|內湖區": "#d9a441",
+// "新北市|板橋區": "#8ca6c4",
+// "新竹": "#b89b72",
+// "嘉義": "#9a8f73"
+export const REGION_COLORS = {};
+
+export const DEFAULT_REGION_COLORS = [
+  "#8fa978",
+  "#98af82",
+  "#81986d",
+  "#a4b88d",
+  "#91a87d",
+  "#879f73"
+];
